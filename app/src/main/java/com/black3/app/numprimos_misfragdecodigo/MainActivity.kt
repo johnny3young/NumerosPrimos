f8db78591f002884d344c406a62f7d559671db19
Johnny3young <youngerblack@gmail.com>
2019-10-24 13:26:30 -0500

    código completo de los números primos

--- app/src/main/java/com/black3/app/numprimos_misfragdecodigo/MainActivity.kt
+++ app/src/main/java/com/black3/app/numprimos_misfragdecodigo/MainActivity.kt
@@ -1,29 +1,46 @@
 package com.black3.app.numprimos_misfragdecodigo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var numeroValidar: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val numEvaluar = editText_Numero?.toString()
-        val a = 1 + 2
-        val b = 4-2
-        val c = 10 % 0
+
         btn_validar.setOnClickListener {
-            if (numEvaluar % 2 == 0) {
-                println("El número no es primo")
-            } else {
-                val iterar = numEvaluar + 1
-            }
+            numeroValidar = editText_Numero.text.toString().toInt()
+            esPrimo(numeroValidar!!)
         }
 
+    }
 
+    fun esPrimo(numeroValidar: Int): Boolean {
+        // El 0, 1 y 4 no son primos
+        if (numeroValidar == 0 || numeroValidar == 1 || numeroValidar == 4) {
+            println("El número $numeroValidar no es Primo")
+
+            return false
+        }
+        for (x in 2 until numeroValidar / 2) {
+            // Si es divisible por cualquiera de estos números, no
+            // es primo
+
+            if (numeroValidar % x == 0){
+                println("El número $numeroValidar no es Primo")
+                return false
+            }
+
+        }
+        // Si no se pudo dividir por ninguno de los de arriba, sí es primo
+        println("El número $numeroValidar es Primo")
+
+        return true
     }
 
 }
--- app/src/main/java/com/black3/app/numprimos_misfragdecodigo/validarNumero.kt
+++ app/src/main/java/com/black3/app/numprimos_misfragdecodigo/validarNumero.kt
@@ -1,8 +1,7 @@
 package com.black3.app.numprimos_misfragdecodigo
 
-
-val deMainActivity = MainActivity()
-
 class validarNumero {
+
+
 
 }
